05d90afb360b75204a97c0a480731e25d1fc18bf
utkarshcs18 <utkarshkumar.cs18@gmail.com>
2026-05-11 04:18:14 +0530

    left with mail

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+if (history.scrollRestoration) {
+  history.scrollRestoration = 'manual';
+}
+window.scrollTo(0, 0);
+
 const mainNav = document.getElementById('mainNav');
 
 window.addEventListener('scroll', () => {
